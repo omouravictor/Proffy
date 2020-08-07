d9b435e139c133d2d5f7c2d593bd3f0b9dd78eac
Victor Moura <victorgabriel.moura@hotmail.com>
2020-08-06 23:23:31 -0300

    Day 3

---
+++ scripts/addField.js
@@ -0,0 +1,10 @@
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+function cloneField () {
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) // cloneNode faz pegar todo o conteúdo dentro do .schedule-item
+    const fields = newFieldContainer.querySelectorAll('input')
+    fields.forEach(function(field) {
+        field.value = ''
+    })
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
