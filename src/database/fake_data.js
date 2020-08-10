c87d20031167610cf774db3d46aecd6ed5bbaa38
Victor Moura <victorgabriel.moura@hotmail.com>
2020-08-09 23:42:40 -0300

    Day 5

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    {
+        name: "Diego Fernanders",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9887569889",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9887569889",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Victor Gabriel",
+        avatar: "https://avatars2.githubusercontent.com/u/64164023?s=460&u=be6ef000da9b04f3189d340c4ac10471b54e5996&v=4",
+        whatsapp: "31985916080",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,96 +1,13 @@
-// Dados
-const proffys = [
-    {
-        name: "Diego Fernanders",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "9887569889",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "9887569889",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Victor Gabriel",
-        avatar: "https://avatars2.githubusercontent.com/u/64164023?s=460&u=be6ef000da9b04f3189d340c4ac10471b54e5996&v=4",
-        whatsapp: "31985916080",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber){
-    const position = subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query // pega os dados da pagina study
-    return res.render("study.html", { proffys, filters, subjects, weekdays }) // retorna dados para página study
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
 // Servidor
 const express = require('express')
 const server = express()
+
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
 // Configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
@@ -101,11 +18,17 @@ nunjucks.configure('src/views', {
 
 // Início e configuração do servidor
 server
+    // receber os dados do req.body
+    .use(express.urlencoded({ extended: true }))
+
     // Configurar arquivos estáticos (css, scripts, imagens)
     .use(express.static("public"))
+
     // rotas da aplicação
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
+
     // Start do servidor
     .listen(5500)
